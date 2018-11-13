c5dec31cf515d39f79eb1e26ebcf532cb7cbd9c6
lisongpo168 <44965274+lisongpo168@users.noreply.github.com>
2018-11-13 17:48:15 +0800

    Add files via upload

---
+++ DataCenter/DataHelper.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCommunity
+{
+    public class DataHelper
+    {
+        public static DataTable GetDataTable(string query)
+        {
+            String conStr = global::MyCommunity.Properties.Settings.Default.DBCommunityConnectionString;
+            DataTable dt = new DataTable();
+            using (MySqlConnection conn = new MySqlConnection(conStr))
+            { 
+                conn.Open(); 
+                using (MySqlCommand cmd = conn.CreateCommand()) 
+                { 
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query; 
+                    MySqlDataAdapter sda = new MySqlDataAdapter(cmd); 
+                    sda.Fill(dt); 
+                } 
+            } 
+            return dt;
+        }
+
+        public static void UpdateOrDeleteRecord(string query)
+        {
+            String conStr = global::MyCommunity.Properties.Settings.Default.DBCommunityConnectionString;
+            DataTable dt = new DataTable();
+            using (MySqlConnection conn = new MySqlConnection(conStr))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
