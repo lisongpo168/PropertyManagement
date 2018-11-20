7ad532402df635fa8df59d4bb3c1d6d4b3a1cf8f
lisongpo168 <44965274+lisongpo168@users.noreply.github.com>
2018-11-20 13:55:15 +0800

    Add files via upload

---
+++ SecurityForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MyCommunity
+{
+    partial class SecurityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTip = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(100, 54);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(259, 15);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "正在进行软件系统使用权限检测，请稍后．．．";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.Location = new System.Drawing.Point(100, 75);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(0, 13);
+            this.lblTip.TabIndex = 1;
+            // 
+            // SecurityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 179);
+            this.Controls.Add(this.lblTip);
+            this.Controls.Add(this.lblInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "SecurityForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SecurityForm";
+            this.Load += new System.EventHandler(this.SecurityForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTip;
+    }
+}
---
+++ SecurityForm.cs
@@ -0,0 +1,66 @@
+﻿using MyCommunity.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCommunity
+{
+    public partial class SecurityForm : DevComponents.DotNetBar.RibbonForm
+    {
+        private int nSecs = 9;
+        private bool bStartTimer = false;
+        public SecurityForm()
+        {
+            InitializeComponent();
+            this.timer1.Start();
+        }
+
+        private void SecurityForm_Load(object sender, EventArgs e)
+        {
+            Thread t = new Thread(CheckSecurity);
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        private void CheckSecurity()
+        {
+            string appSecurityId = ConfigurationManager.AppSettings["SecurityId"];
+            if (!SecurityHelper.CheckSecurityId(appSecurityId))
+            {
+                bStartTimer = true;
+            }
+            else
+            {
+                this.DialogResult = System.Windows.Forms.DialogResult.OK;
+                bStartTimer = false;
+                nSecs = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (nSecs == 0 && bStartTimer)
+            {
+                this.timer1.Stop();
+                this.Close();
+            }
+            if (nSecs > 0 && bStartTimer)
+            {
+                nSecs--;
+                if (nSecs > 0)
+                {
+                    lblInfo.Text = string.Format("当前用户未获得该软件使用权，请联系软件供应商");
+                    lblTip.Text = string.Format("获得正式使用权！窗口将在{0}秒后关闭．．．", nSecs);
+                }
+            }
+        }
+    }
+}
